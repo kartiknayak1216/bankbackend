4a5a0dd87f0264ba669a57a76ed42d5aab21c113
kartiknayak1216 <nayakkartik1216@gmail.com>
2024-06-26 12:42:30 +0530

    build command2

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,42 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 import express from 'express';
-import http from "http";
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import compression from "compression"
-import cors from "cors"
-import dotenv from "dotenv"
-import  depositeroute from "./controller/deposite"
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+
+import depositeroute from './controller/deposite';
 import userRoute from './controller/user';
 import transferroute from './controller/transfer';
 import withdrawroute from './controller/withdraw';
 import transactionroute from './controller/transaction';
-dotenv.config();
-
 
 const app = express();
+
 app.use(cors({
-  credentials:true
-}))
+  credentials: true,
+  origin: true 
+}));
 app.use(compression());
 app.use(cookieParser());
-app.use(bodyParser.json())
-app.use("/api",depositeroute)
-app.use("/api",userRoute)
-app.use("/api", withdrawroute)
-app.use("/api",transferroute)
-app.use("/api",transactionroute)
+app.use(bodyParser.json());
 
 
+app.use('/api', depositeroute);
+app.use('/api', userRoute);
+app.use('/api', withdrawroute);
+app.use('/api', transferroute);
+app.use('/api', transactionroute);
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000; 
 
-
-const server =http.createServer(app)
+const server = http.createServer(app);
 server.listen(port, () => {
-  console.log(`server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
